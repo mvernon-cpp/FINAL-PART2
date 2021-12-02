488a51a00f74c8df059aefe018f639e89310e7a2
Moina Vernon <mvernon@cpp.edu>
2021-12-02 07:44:55 +0000

    Imp atexit

--- kilo.c
+++ kilo.c
@@ -1,12 +1,22 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-	struct termios raw;
-	tcgetattr(STDIN_FILENO, &raw);
-	raw.c_lflag &= ~(ECHO);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 
 int main()
 {
