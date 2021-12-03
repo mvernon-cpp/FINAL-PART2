029660545affd3559309f1f4aaff7479f5f1391d
Moina Vernon <mvernon@cpp.edu>
2021-12-03 18:32:37 +0000

    Imp filerow

--- kilo.c
+++ kilo.c
@@ -39,6 +39,7 @@ typedef struct erow
 struct editorConfig
 {
 	int cx, cy;
+	int rowoff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -267,7 +268,8 @@ void editorDrawRows(struct abuf *ab)
 	int y;
 	for (y = 0; y < E.screenrows; y++)
 	{
-		if (y >= E.numrows)
+		int filerow = y + E.rowoff;
+		if (filerow >= E.numrows)
 		{
 			if (E.numrows == 0 && y == E.screenrows / 3)
 			{
@@ -294,10 +296,10 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[y].size;
+			int len = E.row[filerow].size;
 			if (len > E.screencols)
 				len = E.screencols;
-			abAppend(ab, E.row[y].chars, len);
+			abAppend(ab, E.row[filerow].chars, len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1)
@@ -397,6 +399,7 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.rowoff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
