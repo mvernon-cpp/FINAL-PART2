09c064af28755dd8816dc16a91c925155b9f57ad
Moina Vernon <mvernon@cpp.edu>
2021-12-03 02:21:37 +0000

    Imp clear screen

--- kilo.c
+++ kilo.c
@@ -43,6 +43,36 @@ void enableRawMode()
 		die("tcsetattr");
 }
 
+char editorReadKey()
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+}
+
+/*** output ***/
+void editorRefreshScreen()
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
+/*** input ***/
+void editorProcessKeypress()
+{
+	char c = editorReadKey();
+	switch (c)
+	{
+	case CTRL_KEY('q'):
+		exit(0);
+		break;
+	}
+}
+
 /*** init ***/
 int main()
 {
@@ -50,19 +80,8 @@ int main()
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("read");
-		if (iscntrl(c))
-		{
-			printf("%d\r\n", c);
-		}
-		else
-		{
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == CTRL_KEY('q'))
-			break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 
 	return 0;
