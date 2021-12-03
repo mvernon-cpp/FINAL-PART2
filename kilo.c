b62780316155ff2c253bee03c8b1f701684f4d6d
Moina Vernon <mvernon@cpp.edu>
2021-12-03 02:27:03 +0000

    Imp cursor home & clean exit

--- kilo.c
+++ kilo.c
@@ -15,6 +15,9 @@ struct termios orig_termios;
 /*** terminal ***/
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -59,6 +62,7 @@ char editorReadKey()
 void editorRefreshScreen()
 {
 	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
@@ -68,6 +72,8 @@ void editorProcessKeypress()
 	switch (c)
 	{
 	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
 	}
