14fce5b5d968ce7be021c2be53bae4d89f3391d9
Moina Vernon <mvernon@cpp.edu>
2021-12-03 23:08:41 +0000

    Imp scroll limits

--- kilo.c
+++ kilo.c
@@ -354,6 +354,8 @@ void editorRefreshScreen()
 /*** input ***/
 void editorMoveCursor(int key)
 {
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch (key)
 	{
 	case ARROW_LEFT:
@@ -363,9 +365,10 @@ void editorMoveCursor(int key)
 		}
 		break;
 	case ARROW_RIGHT:
-		
+		if (row && E.cx < row->size)
+		{
 			E.cx++;
-		
+		}
 		break;
 	case ARROW_UP:
 		if (E.cy != 0)
