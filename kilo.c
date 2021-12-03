69fb7a61e6dca385fdca17f4da9f817dda2dcc18
Moina Vernon <mvernon@cpp.edu>
2021-12-03 04:05:48 +0000

    Detect page up & down

--- kilo.c
+++ kilo.c
@@ -16,7 +16,9 @@ enum editorKey
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,	//1001
 	ARROW_UP,		//1002
-	ARROW_DOWN		//1003
+	ARROW_DOWN,		//1003
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** data ***/
@@ -81,18 +83,37 @@ int editorReadKey()
 			return '\x1b';
 		if (seq[0] == '[')
 		{
-			switch (seq[1])
+			if (seq[1] >= '0' && seq[1] <= '9')
 			{
-			case 'A':
-				return ARROW_UP;
-			case 'B':
-				return ARROW_DOWN;
-			case 'C':
-				return ARROW_RIGHT;
-			case 'D':
-				return ARROW_LEFT;
+				if (read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+				if (seq[2] == '~')
+				{
+					switch (seq[1])
+					{
+					case '5':
+						return PAGE_UP;
+					case '6':
+						return PAGE_DOWN;
+					}
+				}
+			}
+			else
+			{
+				switch (seq[1])
+				{
+				case 'A':
+					return ARROW_UP;
+				case 'B':
+					return ARROW_DOWN;
+				case 'C':
+					return ARROW_RIGHT;
+				case 'D':
+					return ARROW_LEFT;
+				}
 			}
 		}
+
 		return '\x1b';
 	}
 	else
@@ -262,6 +283,15 @@ void editorProcessKeypress()
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
+		break;
+
+	case PAGE_UP:
+	case PAGE_DOWN:
+	{
+		int times = E.screenrows;
+		while (times--)
+			editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	}
 		break;
 
 	case ARROW_UP:
