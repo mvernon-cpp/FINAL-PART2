6672e6ff582df04ec45a8aba754742d82045a06b
Moina Vernon <mvernon@cpp.edu>
2021-12-03 18:43:02 +0000

    Added col off

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ struct editorConfig
 {
 	int cx, cy;
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -412,6 +413,7 @@ void initEditor()
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
