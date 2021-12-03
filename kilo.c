c417bbc562c6db55331aac71402e1cd36783e249
Moina Vernon <mvernon@cpp.edu>
2021-12-03 18:47:30 +0000

    Enable horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -274,6 +274,14 @@ void editorScroll()
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff)
+	{
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols)
+	{
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 void editorDrawRows(struct abuf *ab)
 {
@@ -308,10 +316,12 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if(len < 0)
+				len = 0;
 			if (len > E.screencols)
 				len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1)
@@ -331,7 +341,8 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+															(E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -352,10 +363,9 @@ void editorMoveCursor(int key)
 		}
 		break;
 	case ARROW_RIGHT:
-		if (E.cx != E.screencols - 1)
-		{
+		
 			E.cx++;
-		}
+		
 		break;
 	case ARROW_UP:
 		if (E.cy != 0)
