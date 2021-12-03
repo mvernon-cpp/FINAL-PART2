45a38ad380ae32c5f56ab8be33350dad4715c39c
Moina Vernon <mvernon@cpp.edu>
2021-12-03 18:12:22 +0000

    Imp file open

--- kilo.c
+++ kilo.c
@@ -201,15 +201,28 @@ int getWindowSize(int *rows, int *cols)
 }
 
 /*** file i/o ***/
-void editorOpen()
+void editorOpen(char *filename)
 {
-	char *line = "Hello, world!";
-	ssize_t linelen = 13;
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars, line, linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;
+	FILE *fp = fopen(filename, "r");
+	if (!fp)
+		die("fopen");
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1)
+	{
+		while (linelen > 0 && (line[linelen - 1] == '\n' ||
+									  line[linelen - 1] == '\r'))
+			linelen--;
+		E.row.size = linelen;
+		E.row.chars = malloc(linelen + 1);
+		memcpy(E.row.chars, line, linelen);
+		E.row.chars[linelen] = '\0';
+		E.numrows = 1;
+	}
+	free(line);
+	fclose(fp);
 }
 
 /*** append buffer ***/
@@ -376,12 +389,14 @@ void initEditor()
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 }
-int main()
+int main(int argc, char *argv[])
 {
 	enableRawMode();
 	initEditor();
-	editorOpen();
-
+	if (argv >= 2)
+	{
+		editorOpen(argv[1]);
+	}
 	while (1)
 	{
 		editorRefreshScreen();
