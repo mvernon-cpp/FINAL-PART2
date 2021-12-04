010979ea431c5726e2b9b723aa57848f2985f084
Moina Vernon <mvernon@cpp.edu>
2021-12-04 08:16:34 +0000

    Imp status bar

--- kilo.c
+++ kilo.c
@@ -50,6 +50,7 @@ struct editorConfig
 	int screencols;
 	int numrows;
 	erow *row;
+	char *filename;
 	struct termios orig_termios;
 };
 struct editorConfig E;
@@ -265,6 +266,10 @@ void editorAppendRow(char *s, size_t len)
 /*** file i/o ***/
 void editorOpen(char *filename)
 {
+
+	free(E.filename);
+	E.filename = strdup(filename);
+
 	FILE *fp = fopen(filename, "r");
 	if (!fp)
 		die("fopen");
@@ -381,11 +386,26 @@ void editorDrawRows(struct abuf *ab)
 void editorDrawStatusBar(struct abuf *ab)
 {
 	abAppend(ab, "\x1b[7m", 4);
-	int len = 0;
+	char status[80], rstatus[80];
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+							 E.filename ? E.filename : "[No Name]", E.numrows);
+	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+							  E.cy + 1, E.numrows);
+	if (len > E.screencols)
+		len = E.screencols;
+	abAppend(ab, status, len);
 	while (len < E.screencols)
 	{
-		abAppend(ab, " ", 1);
-		len++;
+		if (E.screencols - len == rlen)
+		{
+			abAppend(ab, rstatus, rlen);
+			break;
+		}
+		else
+		{
+			abAppend(ab, " ", 1);
+			len++;
+		}
 	}
 	abAppend(ab, "\x1b[m", 3);
 }
@@ -519,6 +539,8 @@ void initEditor()
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.filename = NULL;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 	E.screenrows -= 1;
