e0229f10edf8a522e3d93b00aadd36bb8905adad
Moina Vernon <mvernon@cpp.edu>
2021-12-04 08:39:49 +0000

    Imp save

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -334,6 +335,18 @@ void editorOpen(char *filename)
 		free(line);
 		fclose(fp);
 	}
+}
+void editorSave()
+{
+	if (E.filename == NULL)
+		return;
+	int len;
+	char *buf = editorRowsToString(&len);
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, len);
+	write(fd, buf, len);
+	close(fd);
+	free(buf);
 }
 
 /*** append buffer ***/
