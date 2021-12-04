462f3fb40474cd1757112414656b2aaf4d1dc28f
Moina Vernon <mvernon@cpp.edu>
2021-12-04 08:39:00 +0000

    Imp rows to strings

--- kilo.c
+++ kilo.c
@@ -290,6 +290,24 @@ void editorInsertChar(int c)
 }
 
 /*** file i/o ***/
+char *editorRowsToString(int *buflen)
+{
+	int totlen = 0;
+	int j;
+	for (j = 0; j < E.numrows; j++)
+		totlen += E.row[j].size + 1;
+	*buflen = totlen;
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for (j = 0; j < E.numrows; j++)
+	{
+		memcpy(p, E.row[j].chars, E.row[j].size);
+		p += E.row[j].size;
+		*p = '\n';
+		p++;
+	}
+	return buf;
+}
 void editorOpen(char *filename)
 {
 
@@ -584,7 +602,7 @@ void editorProcessKeypress()
 	case CTRL_KEY('l'):
 	case '\x1b':
 		break;
-		
+
 	default:
 		editorInsertChar(c);
 		break;
