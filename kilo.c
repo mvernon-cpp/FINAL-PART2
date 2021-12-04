be5df2b695e0dd03030020d1901bbffbc0c4cb80
Moina Vernon <mvernon@cpp.edu>
2021-12-04 08:58:04 +0000

    Imp dirty to detect unsaved file changes

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ struct editorConfig
 	int screencols;
 	int numrows;
 	erow *row;
+	int dirty;
 	char *filename;
 	char statusmsg[80];
 	time_t statusmsg_time;
@@ -270,6 +271,7 @@ void editorAppendRow(char *s, size_t len)
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
+	E.dirty++;
 }
 void editorRowInsertChar(erow *row, int at, int c)
 {
@@ -280,6 +282,7 @@ void editorRowInsertChar(erow *row, int at, int c)
 	row->size++;
 	row->chars[at] = c;
 	editorUpdateRow(row);
+	E.dirty++;
 }
 
 /*** editor operations ***/
@@ -337,6 +340,7 @@ void editorOpen(char *filename)
 		}
 		free(line);
 		fclose(fp);
+		E.dirty = 0;
 	}
 }
 void editorSave()
@@ -354,6 +358,8 @@ void editorSave()
 			{
 				close(fd);
 				free(buf);
+				E.dirty = 0;
+
 				editorSetStatusMessage("%d bytes written to disk", len);
 				return;
 			}
@@ -459,8 +465,10 @@ void editorDrawStatusBar(struct abuf *ab)
 {
 	abAppend(ab, "\x1b[7m", 4);
 	char status[80], rstatus[80];
-	int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-							 E.filename ? E.filename : "[No Name]", E.numrows);
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+							 E.filename ? E.filename : "[No Name]", E.numrows,
+							 E.dirty ? "(modified)" : "");
+	
 	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
 							  E.cy + 1, E.numrows);
 	if (len > E.screencols)
@@ -651,6 +659,7 @@ void initEditor()
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.dirty = 0;
 	E.filename = NULL;
 	E.statusmsg[0] = '\0';
 	E.statusmsg_time = 0;
