21962fc9b9819a3e1bc1b4c2fbed877bdcdcae37
Moina Vernon <mvernon@cpp.edu>
2021-12-04 09:12:28 +0000

    Imp row append string

--- kilo.c
+++ kilo.c
@@ -298,6 +298,15 @@ void editorRowInsertChar(erow *row, int at, int c)
 	editorUpdateRow(row);
 	E.dirty++;
 }
+void editorRowAppendString(erow *row, char *s, size_t len)
+{
+	row->chars = realloc(row->chars, row->size + len + 1);
+	memcpy(&row->chars[row->size], s, len);
+	row->size += len;
+	row->chars[row->size] = '\0';
+	editorUpdateRow(row);
+	E.dirty++;
+}
 void editorRowDelChar(erow *row, int at)
 {
 	if (at < 0 || at >= row->size)
@@ -329,6 +338,7 @@ void editorDelChar()
 		E.cx--;
 	}
 }
+
 /*** file i/o ***/
 char *editorRowsToString(int *buflen)
 {
