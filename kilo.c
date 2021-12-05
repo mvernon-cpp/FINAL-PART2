d5b2c43375133160082ad447b59725651e549a22
Moina Vernon <mvernon@cpp.edu>
2021-12-05 15:16:13 +0000

    Imp syntax refactoring

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow
 	int rsize;
 	char *chars;
 	char *render;
+	unsigned char *hl;
 } erow;
 
 struct editorConfig
@@ -275,7 +276,6 @@ void editorUpdateRow(erow *row)
 }
 void editorInsertRow(int at, char *s, size_t len)
 {
-
 	if (at < 0 || at > E.numrows)
 		return;
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -288,6 +288,7 @@ void editorInsertRow(int at, char *s, size_t len)
 
 	E.row[at].rsize = 0;
 	E.row[at].render = NULL;
+	E.row[at].hl = NULL;
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
@@ -297,6 +298,7 @@ void editorFreeRow(erow *row)
 {
 	free(row->render);
 	free(row->chars);
+	free(row->hl);
 }
 void editorDelRow(int at)
 {
