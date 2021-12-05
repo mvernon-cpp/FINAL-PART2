c43e75281a58939e3ec3e97d8503e78fec6ee1c6
Moina Vernon <mvernon@cpp.edu>
2021-12-05 22:15:42 +0000

    Imp string escapes

--- kilo.c
+++ kilo.c
@@ -274,6 +274,12 @@ void editorUpdateSyntax(erow *row)
 			if (in_string)
 			{
 				row->hl[i] = HL_STRING;
+				if (c == '\\' && i + 1 < row->rsize)
+				{
+					row->hl[i + 1] = HL_STRING;
+					i += 2;
+					continue;
+				}
 				if (c == in_string)
 					in_string = 0;
 				i++;
@@ -291,7 +297,7 @@ void editorUpdateSyntax(erow *row)
 				}
 			}
 		}
-		
+
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
 		{
 			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
