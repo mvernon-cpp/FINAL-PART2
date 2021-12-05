94834a1c07457fdbc7f5a38a25b249cdddef6820
Moina Vernon <mvernon@cpp.edu>
2021-12-05 20:29:07 +0000

    Imp use filetype

--- kilo.c
+++ kilo.c
@@ -254,21 +254,27 @@ void editorUpdateSyntax(erow *row)
 	row->hl = realloc(row->hl, row->rsize);
 	memset(row->hl, HL_NORMAL, row->rsize);
 
+	if (E.syntax == NULL)
+		return;
+
 	int prev_sep = 1;
 	int i = 0;
 
-	while(i < row->size)
+	while (i < row->size)
 	{
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-		if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
-        (c == '.' && prev_hl == HL_NUMBER))
+		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
 		{
-			row->hl[i] = HL_NUMBER;
-			i++;
-			prev_sep = 0;
-			continue;
+			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+				 (c == '.' && prev_hl == HL_NUMBER))
+			{
+				row->hl[i] = HL_NUMBER;
+				i++;
+				prev_sep = 0;
+				continue;
+			}
 		}
 		prev_sep = is_separator(c);
 		i++;
@@ -594,7 +600,7 @@ void editorFindCallback(char *query, int key)
 			E.cy = current;
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
-			
+
 			saved_hl_line = current;
 			saved_hl = malloc(row->rsize);
 			memcpy(saved_hl, row->hl, row->rsize);
