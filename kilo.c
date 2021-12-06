0465d2b5c88c8379f13c879b14aad999768b7c05
Moina Vernon <mvernon@cpp.edu>
2021-12-06 03:38:54 +0000

    Dealing with nonprintables

--- kilo.c
+++ kilo.c
@@ -848,14 +848,24 @@ void editorDrawRows(struct abuf *ab)
 			int j;
 			for (j = 0; j < len; j++)
 			{
-				if (hl[j] == HL_NORMAL)
+				if (iscntrl(c[j]))
 				{
-					if (current_color != -1)
+					char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+					abAppend(ab, "\x1b[7m", 4);
+					abAppend(ab, &sym, 1);
+					abAppend(ab, "\x1b[m", 3);
+				}
+				else if (hl[j] == HL_NORMAL)
+				{
+					if (hl[j] == HL_NORMAL)
 					{
-						abAppend(ab, "\x1b[39m", 5);
-						current_color = -1;
+						if (current_color != -1)
+						{
+							abAppend(ab, "\x1b[39m", 5);
+							current_color = -1;
+						}
+						abAppend(ab, &c[j], 1);
 					}
-					abAppend(ab, &c[j], 1);
 				}
 				else
 				{
